ae6ed51fa949cc41e694ed2abb761b82550d3d64
jens <asdf@asdf.com>
2015-10-09 17:20:47 -0700

    added matchmaking -group formation basics

---
+++ Web/WindowsDefender_WebApp - Copy/WindowsDefender_WebApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WindowsDefender_WebApp.Startup))]
+namespace WindowsDefender_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
