de78e44c10253f7e8c2ed5295ee718a70303fd53
unknown <student@bcit.ca>
2015-10-20 17:25:25 -0700

    norton aoe tower

---
+++ Unity Project/TopDownView/Assets/Towers/NortonTowerAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NortonTowerAI : TowerAI
+{
+
+    NortonTowerAI() : base()
+    {
+        towerDamage = 50;
+        attackSpd = 0.5f;
+        attacksGround = true;
+        attacksAir = false;
+        drainDamage = 20;
+        drainSpd = 1.6f;
+        drainDuration = 2.0f;
+    }
+
+    public override projectileAI shootTarget(GameObject target)
+    {
+        projectileAI temp = base.shootTarget(target);
+
+        temp.aoeTarget = target.transform.position;
+
+        return temp;
+    }
+
+
+
+}
