83650f7957d381c8b055893780c240fb9da8f2e0
Darnell Andries <dandries@my.bcit.ca>
2015-10-30 16:29:14 -0700

    added mouse navigation

--- Unity Project/TopDownView/Assets/Components/CameraAI.cs
+++ Unity Project/TopDownView/Assets/Components/CameraAI.cs
@@ -7,11 +7,10 @@ public class CameraAI : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 direction = transform.position;
 
+    Vector3 KeyboardNavigation(Vector3 position)
+    {
+        Vector3 direction = position;
         if (Input.GetKey(KeyCode.W))
             direction.y += 2;
         if (Input.GetKey(KeyCode.A))
@@ -20,7 +19,27 @@ public class CameraAI : MonoBehaviour {
             direction.y -= 2;
         if (Input.GetKey(KeyCode.D))
             direction.x += 2;
+        return direction;
+    }
 
+    Vector3 MouseNavigation(Vector3 position)
+    {
+        Vector3 direction = position;
+        if (Input.mousePosition.y < 30)
+            direction.y -= 5;
+        if (Input.mousePosition.x < 30)
+            direction.x -= 5;
+        if (Input.mousePosition.y > (Display.main.renderingHeight - 30))
+            direction.y += 5;
+        if (Input.mousePosition.x > (Display.main.renderingWidth - 30))
+            direction.x += 5;
+        return direction;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 direction = KeyboardNavigation(transform.position);
+        Vector3 direction = MouseNavigation(transform.position);
         transform.position = Vector3.MoveTowards(transform.position, direction, 1.0f * Time.deltaTime);
     }
 }
