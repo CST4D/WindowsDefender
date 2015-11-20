7c0a1ad4374694d9895e3e09712d6bee4e98366a
Darnell Andries <dandries@my.bcit.ca>
2015-11-20 11:35:03 -0800

    Documentation of code and sample uri handlers

--- Unity Project/TopDownView/Assets/Components/SpawnerAI.cs
+++ Unity Project/TopDownView/Assets/Components/SpawnerAI.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+/// <summary>
+/// 
+/// </summary>
 public class SpawnerAI : MonoBehaviour {
+    /// <summary>
+    /// The way points
+    /// </summary>
     public LinkedList<Vector2> wayPoints;
+    /// <summary>
+    /// The fly points
+    /// </summary>
     public LinkedList<Vector2> flyPoints;
+    /// <summary>
+    /// The target waypoint
+    /// </summary>
     public Waypoint targetWaypoint;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    /// <summary>
+    /// Starts this instance.
+    /// </summary>
+    void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    /// <summary>
+    /// Updates this instance.
+    /// </summary>
+    void Update () {
 	
 	}
 }
